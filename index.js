231737cb1be24587ae3056a64707333bfdf6fca1
Rahiman Shaik <rahimanshaik13@gmail.com>
2024-09-02 16:18:47 +0530

    changes

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const patientRoutes = require('./routes/patientRoutes');
 const bodyParser = require('body-parser');
-//const Clinisist = require('./models/Clinisist');
 const clinisistRoutes = require('./routes/clinisistRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const passport = require('passport');
@@ -16,6 +15,7 @@ const organizationRoutes = require('./routes/organizationRoutes');
 const orgAdmin = require('./routes/orgAdminRoutes');
 const manager = require('./routes/managerRoutes');
 const forgot = require('./routes/forgotPassword');
+const orgClinisist = require('./routes/orgClinisistRoutes');
 const cors = require('cors');
 
 dotenv.config();
@@ -23,6 +23,17 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+// Define CORS options
+const corsOptions = {
+  origin: '*', // Replace '*' with specific origin(s) if needed, e.g., 'http://example.com'
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // If you need to expose credentials (cookies, authorization headers) to the client
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(passport.initialize());
@@ -38,11 +49,11 @@ app.use('/api/privacy', privacy);
 app.use('/api/organization', organizationRoutes);
 app.use('/api/orgadmin', orgAdmin);
 app.use('/api/manager', manager);
-app.use('/api',forgot);
-app.use(cors());
+app.use('/api', forgot);
+app.use('/api', orgClinisist);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`app running on ${PORT}`);
+  console.log(`App running on port ${PORT}`);
 });
